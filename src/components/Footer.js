22eae8b635abdf081fd0be54c19a8822e6577a7f
Coumba Ngom <kumbugaan@gmail.com>
2022-10-14 20:46:38 +0000

    ...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,12 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 export class Footer extends Component {
   render() {
     return (
-      <div className='container-fluid footer pt-4 mt-5 bg-dark'>
+      <div className='container-fluid footer pt-4 mt-5 bg-dark text-center'>
         <div className='row bg-dark '>
-        <div className='col-md-9 col-sm-12 col-xs-12'>
+        <div className='col-md-6 col-sm-12 col-xs-12'>
             <img src={image5} alt="" className='f-job pb-4'/>
         </div>
-        <div className='col-md-3 col-sm-12 col-xs-12 text-sm '>
+        <div className='col-md-6 col-sm-12 col-xs-12 text-sm '>
           <div className='f-droite pt-4'>
           {/* <button type='button' className='bg-white border-0'>
                 <img src={FRA} alt="" />
@@ -25,38 +25,38 @@ export class Footer extends Component {
             </button> */}
             <span>
             <img src={Vector} alt="" className='f-logo'/>
-            <img src={Vector2} alt="" className='f-logo'/>
-            <img src={Vector1} alt="" className='f-logo'/>
+            <img src={Vector2} alt="" className='f-logo ms-2'/>
+            <img src={Vector1} alt="" className='f-logo ms-2'/>
             </span>
           </div>    
         </div>
         </div> 
-        <div className='row bg-dark text-white pt-4  text-center'>
+        <div className='row bg-dark text-white pt-4 text-center'>
         <hr className='footer-hr'/>
-        <div className='col-md-3 col-sm-12 col-xs-12'>
+        <div className='col-md-3 col-sm-12 col-xs-12 mt-4'>
             <p>S'inscrire<br/>
             Chercher une offre<br/>
             Découvrir les entreprises<br/>
             Evénements recrutement<br/>
             Conseils recrutement</p>
         </div>
-        <div className='col-md-3 col-sm-12 col-xs-12'>
+        <div className='col-md-3 col-sm-12 col-xs-12 mt-4'>
             <p>Écoles & Universités<br/>
             Notre offre Career Center<br/>
             Nos établissements partenaires</p>
         </div>
-        <div className='col-md-3 col-sm-12 col-xs-12'>
+        <div className='col-md-3 col-sm-12 col-xs-12 mt-4'>
             <p>Entreprises<br/>
             Notre offre Entreprise</p>
         </div>
-        <div className='col-md-3 col-sm-12 col-xs-12'>
+        <div className='col-md-3 col-sm-12 col-xs-12 mt-4'>
             <p>JobTeaser<br/>
             Nous rejoindre<br/>
             À propos<br/>
             Rencontrer notre équipe</p>
         </div>
         </div>
-           <div className='row bg-dark text-white pt-4'>
+           <div className='row bg-dark text-white pt-4 text-center'>
            <hr className='footer-hr'/>
            <div className='col-md-2 col-sm-12 col-xs-12'>
               <p>Statuts </p>
